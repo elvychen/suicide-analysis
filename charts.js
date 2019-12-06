9732022da8c5de4feadc9acbae736edb69acdce4
yuc399 <yuc399@ucsd.edu>
2019-12-06 14:52:10 -0800

    update

--- charts.js
+++ charts.js
@@ -32,19 +32,11 @@ mouse/touch event handler to bind the charts together.
                 }
                 // Get the hovered point
                 if (point) {
-                    if (scatter.xAxis[0].visible == false){
-                      scatter.xAxis[0].update({visible:true});
-                      scatter.yAxis[0].update({visible:true});
-                    }
                     if (chart.renderTo.id == "timeline"){
                       var data = worldmap[point.id];
                       geomap.setTitle({'text':point.id+' Worldwide Suicide Rate Distribution'});
                       geomap.series[0].update({'data':data});
 
-                      var data_scatter = scatterGDP[point.id];
-                      scatter.update({series: data_scatter});
-                      scatter.setTitle({'text':'GDP Per Capita in Each Country <br> VS.<br> Suicide Rate ( ' +point.id + ' )',
-                      'verticalAlign': 'top'})
                       lineChart.tooltip.update({enabled:true})
                       lineChart.series[0].setData(getRateLine(point.id));
                       wordCloud.setTitle({'text':'Suicide Rate For Each Age Group ( ' + point.id +' )',
@@ -121,7 +113,8 @@ var timeline = {
     chart: {
       zoomType: 'x',
       type: 'timeline',
-      backgroundColor:'transparent'
+      backgroundColor:'transparent',
+      color: 'white',
     },
     xAxis: {
       type: 'datetime',
@@ -129,12 +122,18 @@ var timeline = {
       labels: {
         format: '{value:%Y}'
       },
+      style:{
+        color: '#FFFFFF'
+      }
     },
     yAxis: {
       gridLineWidth: 1,
       title: null,
       labels: {
         enabled: false
+      },
+      style:{
+        color: '#FFFFFF'
       }
     },
     legend: {
@@ -144,24 +143,31 @@ var timeline = {
         enabled: false,
     },
     title: {
-      text: "Timeline For Overal Suicide Rate"
+      text: "Timeline For Overal Suicide Rate",
+      style:{
+        color: '#FFFFFF'
+      }
     },
     subtitle: {
-        text: 'World\'s Overall Suicide Rate From 2005 To 2015'
+        text: 'World\'s Overall Suicide Rate From 2005 To 2015',
+        style:{
+          color: '#FFFFFF'
+        }
     },
     tooltip: {
       enabled: false,
     },
     series: [{
       dataLabels: {
-        allowOverlap: false,
+        allowOverlap: true,
         formatter: function(){
               var date = Highcharts.dateFormat('%Y',this.x);
               // var rate = this.point.label
               return '<span style="color:'+this.point.color+'">● </span><span style="font-weight: bold;" > ' +
                     date+'</span><br/>'+
                     '<span style="font-weight: normal; font-size: 6pt;" > '+'Suicide Rate: <b>'+(this.point.label*100).toFixed(3)+"‱" + "</b></span>";
-        }
+        },
+        
       },
       marker: {
         symbol: 'circle'
@@ -192,7 +198,6 @@ function changeConclusion(){
 var dataset = {};
 var timeline_dict = {};
 var worldmap = {};
-var scatterGDP = {};
 var wordCloudData = {};
 Highcharts.ajax({
     url:'./dataset1.json',
@@ -230,36 +235,15 @@ Highcharts.ajax({
             currCountries = Object.keys(info);
             countries = [];
             countries_g = [];
-            sum = 0;
-            n = currCountries.length;
-            max_d = 0;
-            for (j = 0;j<89;j++){
-                if (j<n){
-                  first_val = parseFloat((parseFloat(info[currCountries[j]][0])*100).toFixed(3));
-                  second_val = parseFloat(info[currCountries[j]][1]);
-                  sum += first_val;
-                  country_s = {};
-                  country_s['name'] = currCountries[j];
-                  country_s['value'] = first_val;
-                  countries.push(country_s);
-                  country_g = {};
-                  country_g['name'] = currCountries[j];
-                  country_g['data'] = [[second_val,first_val]];
-                  countries_g.push(country_g);
-                  if (second_val > max_d){
-                    max_d = second_val;
-                  }
-                }
-                else{
-                  countries_g.push({});
-                }
+            for (j = 0;j<currCountries.length;j++){
+                first_val = parseFloat((parseFloat(info[currCountries[j]][0])*100).toFixed(3));
+                second_val = parseFloat(info[currCountries[j]][1]);
+                country_s = {};
+                country_s['name'] = currCountries[j];
+                country_s['value'] = first_val;
+                countries.push(country_s);             
             }
-            mean = sum/n ;
-            var last = {'type': 'line', 'color':'#000000','name': 'Regression Line',data:[[0,mean],[max_d,mean]],'enableMouseTracking': false,'marker': {
-              'enabled': false},'states': {'hover': {'lineWidth':2}}};
-            countries_g.push(last);
             worldmap[country_year[i]] = countries;
-            scatterGDP[country_year[i]] = countries_g;
         }
         tempWord = activity[2];
         wordCloudData = {};
@@ -288,6 +272,9 @@ geomap = new Highcharts.mapChart('geomap', {
     title: {
         text: 'How Suicide Rate Differs Worldwide<br> From 2005 To 2015?',
         align: 'center',
+        style:{
+          color: '#FFFFFF'
+        }
       },
     mapNavigation: {
       enabled: true,
@@ -298,6 +285,9 @@ geomap = new Highcharts.mapChart('geomap', {
     subtitle:{
         text: "",
         useHTML : true,
+        style:{
+          color: '#FFFFFF'
+        }
     },
     credits:{
         enabled: false,
@@ -306,7 +296,7 @@ geomap = new Highcharts.mapChart('geomap', {
         dataClasses: [{
           color: '#EEE89D',
           to: 0.5,
-          name: '< 0.5‱'
+          name: '< 0.5‱',
       }, {
           color: '#EDCE8D',
           from: 0.5,
@@ -347,34 +337,44 @@ lineChart = Highcharts.chart('lineChart', {
   },
 
   title: {
-      text: 'Suicide Rate\'s Trends From 2005 To 2015 '
+      text: ''
   },
   credits:{
     enabled: false,
   },
   subtitle: {
-      text: 'Source: kaggle.com'
+    enabled: false,
   },
   xAxis:{
-    categories:['2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015']
+    categories:['2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015'],
+    labels:{
+      style:{
+      color: '#FFFFFF'
+    },
+    }
   },
   yAxis: {
       title: {
-          text: 'Suicide Rate (‱)'
+          text: 'Suicide Rate (‱)',
+          style:{
+            color: '#FFFFFF'
+          },
       },
       labels: {
         formatter: function(){
                     return (this.value*100).toFixed(3)+"‱"
-        }
+        },
+        style:{
+          color: '#FFFFFF'
+        },
         
-    },
+      },
+      
       min : 0.011,
       max: 0.0136
   },
   legend: {
-      layout: 'vertical',
-      align: 'center',
-      verticalAlign: 'bottom'
+     enabled: false,
   },
   tooltip: {
     headerFormat: '<b>Year:</b> <span style= "color: #ff0000"> {point.x} </span><br>',
@@ -384,7 +384,7 @@ lineChart = Highcharts.chart('lineChart', {
     enabled: false
 },
 
-  series: [{name:'suicide_rate',data:[0,0,0,0,0,0,0,0,0,0,0]}],
+  series: [{name:'suicide rate (‱)',data:[0,0,0,0,0,0,0,0,0,0,0],color:'red'}],
 
   responsive: {
       rules: [{
@@ -402,80 +402,6 @@ lineChart = Highcharts.chart('lineChart', {
   }
 
 });
-
-scatter = Highcharts.chart('scatter', {
-  chart: {
-      type: 'scatter',
-      zoomType: 'xy',
-      backgroundColor:'transparent'
-  },
-  accessibility: {
-      description: 'A scatter plot compares the height and weight of 507 individuals by gender. Height in centimeters is plotted on the X-axis and weight in kilograms is plotted on the Y-axis. The chart is interactive, and each data point can be hovered over to expose the height and weight data for each individual. The scatter plot is fairly evenly divided by gender with females dominating the left-hand side of the chart and males dominating the right-hand side. The height data for females ranges from 147.2 to 182.9 centimeters with the greatest concentration between 160 and 165 centimeters. The weight data for females ranges from 42 to 105.2 kilograms with the greatest concentration at around 60 kilograms. The height data for males ranges from 157.2 to 198.1 centimeters with the greatest concentration between 175 and 180 centimeters. The weight data for males ranges from 53.9 to 116.4 kilograms with the greatest concentration at around 80 kilograms.'
-  },
-  title: {
-    text: 'Is There a Relationship Between<br><b>GDP per Capita in Each Country</b> and <b>Suicide Rate</b>?',
-    align:'center',
-    verticalAlign:'middle'
-  },
-  subtitle: {
-      enabled: false,
-  },
-  xAxis: {
-      title: {
-          enabled: true,
-          text: 'GDP per capita ($)'
-      },
-      startOnTick: true,
-      endOnTick: true,
-      showLastLabel: true,
-      min:0,
-      visible: false,
-
-  },
-  yAxis: {
-      title: {
-          text: 'suicide rate (‱)'
-      },
-      visible: false,
-  },
-  credits:{
-    enabled: false,
-  },
-  legend: {
-      enabled: false,
-  },
-  plotOptions: {
-      scatter: {
-          marker: {
-              radius: 5,
-              states: {
-                  hover: {
-                      enabled: true,
-                      lineColor: 'rgb(100,100,100)'
-                  }
-              }
-          },
-          states: {
-              hover: {
-                  marker: {
-                      enabled: false
-                  }
-              }
-          },
-          tooltip: {
-              headerFormat: '<b>{series.name}</b><br>',
-              pointFormat: ' $ {point.x}, {point.y} ‱'
-          }
-      }
-  },
-  series: [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {},
-    {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {},
-    {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {},
-    {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {},
-    {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {},
-    {}, {}, {}, {},{}],
-});
-
 
 wordCloud = Highcharts.chart('wordCloud', {
     chart:{
